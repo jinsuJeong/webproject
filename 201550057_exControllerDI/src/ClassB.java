bd762b154e1bb03200b12fc50d36a7b2137abb80
D7608 <D7608@DESKTOP-AL9FBJ7>
2018-10-11 11:57:49 +0900

    10/11 ControllerDI

---
+++ 201550057_exControllerDI/src/ClassB.java
@@ -0,0 +1,11 @@
+
+public class ClassB {
+	ClassA classA;
+	
+	public ClassB(ClassA classA) {
+		this.classA=classA;
+	}
+	public void methodB() {
+		System.out.println("ClassB depends on "+classA.methodA());
+	}
+}
---
+++ 201550057_exControllerDI/src/TestConstructorDI.java
@@ -0,0 +1,9 @@
+
+public class TestConstructorDI {
+	 public static void main(String[] args) {
+		  ClassA classA = new ClassA();
+		  //Constructor based Dependency Injection
+		  ClassB classB = new ClassB(classA);
+		  classB.methodB();
+		 }
+}
